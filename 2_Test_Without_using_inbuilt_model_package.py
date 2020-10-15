2d120ad9be9c9bc716635d6d46a5df8e33aae246
rucha80 <40273668+rucha80@users.noreply.github.com>
2020-10-15 16:18:46 +0530

    Add files via upload

---
+++ 2_Test_Without_using_inbuilt_model_package.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 14 11:25:37 2020
+
+@author: Rucha
+"""
+
+#This code tells you how to implement CNN model without using inbuilt model keras
+#This will give you basic idea of tensorflow layers mplementation
+#I have followed the blog https://towardsdatascience.com/cifar-10-image-classification-in-tensorflow-5b501f7dc77c to implemnt this code
+
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelBinarizer
+
+def batch_features_labels(features, labels, batch_size):
+    """
+    Split features and labels into batches
+    """
+    for start in range(0, len(features), batch_size):
+        end = min(start + batch_size, len(features))
+        yield features[start:end], labels[start:end]
+
+def display_image_predictions(features, labels, predictions, top_n_predictions):
+    n_classes = 10
+    label_names = load_label_names()
+    label_binarizer = LabelBinarizer()
+    label_binarizer.fit(range(n_classes))
+    label_ids = label_binarizer.inverse_transform(np.array(labels))
+
+    fig, axies = plt.subplots(nrows=top_n_predictions, ncols=2, figsize=(20, 10))
+    fig.tight_layout()
+    fig.suptitle('Softmax Predictions', fontsize=20, y=1.1)
+
+    n_predictions = 3
+    margin = 0.05
+    ind = np.arange(n_predictions)
+    width = (1. - 2. * margin) / n_predictions
+   
+    for image_i, (feature, label_id, pred_indicies, pred_values) in enumerate(zip(features, label_ids, predictions.indices, predictions.values)):
+        if (image_i < top_n_predictions):
+            pred_names = [label_names[pred_i] for pred_i in pred_indicies]
+            correct_name = label_names[label_id]
+            
+            axies[image_i][0].imshow((feature*255).astype(np.int32, copy=False))
+            axies[image_i][0].set_title(correct_name)
+            axies[image_i][0].set_axis_off()
+
+            axies[image_i][1].barh(ind + margin, pred_values[:3], width)
+            axies[image_i][1].set_yticks(ind + margin)
+            axies[image_i][1].set_yticklabels(pred_names[::-1])
+            axies[image_i][1].set_xticks([0, 0.5, 1.0])
+            
+%matplotlib inline
+%config InlineBackend.figure_format = 'retina'
+
+import tensorflow as tf
+import pickle
+import random
+
+save_model_path = './image_classification'
+batch_size = 64
+n_samples = 10
+top_n_predictions = 5
+
+def test_model():
+    test_features, test_labels = pickle.load(open('preprocess_training.p', mode='rb'))
+    loaded_graph = tf.Graph()
+
+    with tf.Session(graph=loaded_graph) as sess:
+        # Load model
+        loader = tf.train.import_meta_graph(save_model_path + '.meta')
+        loader.restore(sess, save_model_path)
+
+        # Get Tensors from loaded model
+        loaded_x = loaded_graph.get_tensor_by_name('input_x:0')
+        loaded_y = loaded_graph.get_tensor_by_name('output_y:0')
+        loaded_keep_prob = loaded_graph.get_tensor_by_name('keep_prob:0')
+        loaded_logits = loaded_graph.get_tensor_by_name('logits:0')
+        loaded_acc = loaded_graph.get_tensor_by_name('accuracy:0')
+        
+        # Get accuracy in batches for memory limitations
+        test_batch_acc_total = 0
+        test_batch_count = 0
+        
+        for train_feature_batch, train_label_batch in batch_features_labels(test_features, test_labels, batch_size):
+            test_batch_acc_total += sess.run(
+                loaded_acc,
+                feed_dict={loaded_x: train_feature_batch, loaded_y: train_label_batch, loaded_keep_prob: 1.0})
+            test_batch_count += 1
+
+        print('Testing Accuracy: {}\n'.format(test_batch_acc_total/test_batch_count))
+
+        # Print Random Samples
+        random_test_features, random_test_labels = tuple(zip(*random.sample(list(zip(test_features, test_labels)), n_samples)))
+        random_test_predictions = sess.run(
+            tf.nn.top_k(tf.nn.softmax(loaded_logits), top_n_predictions),
+            feed_dict={loaded_x: random_test_features, loaded_y: random_test_labels, loaded_keep_prob: 1.0})
+        display_image_predictions(random_test_features, random_test_labels, random_test_predictions, top_n_predictions)
+
+
+test_model()
